baf5823a8336eb636e90129e70086d9d48a2de61
zacariasMedina <zacarias_medina@live.cl>
2015-11-25 19:18:24 -0300

    fecha cTM

--- Veranum/src/veranum/DAO/DAOUsuarios.java
+++ Veranum/src/veranum/DAO/DAOUsuarios.java
@@ -17,7 +17,7 @@ import veranum.utilidades.OracleConection;
  */
 public class DAOUsuarios {
     public static boolean sqlInsert(ClPasajeros usuarios) throws SQLException{
-        String sql="INSERT INTO \"pasajeros\" (\"rut\", \"nombre\", \"contrasena\", \"apellido_pa\", \"apellido_ma\", \"telefono\", \"email\", \"direccion\", \"fecha_nac\", \"id_rol\" ) VALUES (?,?,?,?,?,?,?,?,TO_DATE(?, 'dd/MM/YYYY'),?)";
+        String sql="INSERT INTO \"pasajeros\" (\"rut\", \"nombre\", \"contrasena\", \"apellido_pa\", \"apellido_ma\", \"telefono\", \"email\", \"direccion\", \"fecha_nac\", \"id_rol\" ) VALUES (?,?,?,?,?,?,?,?,?,?)";
         PreparedStatement a = OracleConection.getInstance().sqlPreparar(sql);
         a.setString(1, usuarios.getRut());
         a.setString(2, usuarios.getNombre());
@@ -27,7 +27,7 @@ public class DAOUsuarios {
         a.setString(6, usuarios.getTelefono());
         a.setString(7, usuarios.getEmail());
         a.setString(8, usuarios.getDireccion());
-        a.setDate(9, (Date) usuarios.getFechaNacimiento());
+        a.setDate(9, (new java.sql.Date(usuarios.getFechaNacimiento().getTime())));
         a.setInt(10, usuarios.getIdRol());
         OracleConection.getInstance().sqlEjecutarPreparacion();
         return true;
@@ -54,7 +54,7 @@ public class DAOUsuarios {
         a.setString(6, usuarios.getTelefono());
         a.setString(7, usuarios.getEmail());
         a.setString(8, usuarios.getDireccion());
-        a.setDate(9, (Date) usuarios.getFechaNacimiento());
+        a.setDate(9, (new java.sql.Date(usuarios.getFechaNacimiento().getTime())));
         a.setInt(10, usuarios.getIdRol());
         a.setInt(11, usuarios.getIdPasajero());
         OracleConection.getInstance().sqlEjecutarPreparacion();  
