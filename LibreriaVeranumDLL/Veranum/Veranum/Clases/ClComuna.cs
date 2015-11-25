cf643d10d5e853819296dac35fcd1c82ec53011c
zacariasMedina <zacarias_medina@live.cl>
2015-11-25 16:44:17 -0300

    .gitignore is now working

---
+++ LibreriaVeranumDLL/Veranum/Veranum/Clases/ClComuna.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Veranum.Clases
+{
+    class ClComuna
+    {
+        private int idComuna;
+        private String nombre;
+        private int idProvincia;
+
+        public ClComuna()
+        {
+        }
+
+        public ClComuna(int idComuna)
+        {
+            this.IdComuna = idComuna;
+        }
+
+        public ClComuna(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        public ClComuna(int idComuna, string nombre, int idProvincia)
+        {
+            this.IdComuna = idComuna;
+            this.Nombre = nombre;
+            this.IdProvincia = idProvincia;
+        }
+
+        public int IdProvincia
+        {
+            get { return idProvincia; }
+            set { idProvincia = value; }
+        }
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        
+
+        public int IdComuna
+        {
+            get { return idComuna; }
+            set { idComuna = value; }
+        }
+        
+
+    }
+}
---
+++ LibreriaVeranumDLL/Veranum/Veranum/Clases/ClRol.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Veranum.Clases
+{
+    class ClRol
+    {
+        private int idRol;
+        private string nombre;
+        private string descripcion;
+
+        public ClRol()
+        {
+        }
+
+        public ClRol(int idRol)
+        {
+            this.IdRol = idRol;
+        }
+
+        public ClRol(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        public ClRol(int idRol, string nombre, string descripcion)
+        {
+            this.IdRol = idRol;
+            this.Nombre = nombre;
+            this.Descripcion = descripcion;
+        }
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int IdRol
+        {
+          get { return idRol; }
+          set { idRol = value; }
+        }
+
+    }
+}
