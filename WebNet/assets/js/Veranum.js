14c90b5f4376c707d5673423de315f6bc1532bdc
zacariasMedina <zacarias_medina@live.cl>
2015-11-26 15:46:14 -0300

    validacion registro

--- WebNet/assets/js/Veranum.js
+++ WebNet/assets/js/Veranum.js
@@ -15,6 +15,33 @@
         rules: {
             "ctl00$ContentPlaceHolder1$txtRut": {
                 required: true
+            },
+            "ctl00$ContentPlaceHolder1$txtContraseña": {
+                required: true
+            },
+            "ctl00$ContentPlaceHolder1$txtNombre": {
+                required: true
+            },
+            "ctl00$ContentPlaceHolder1$txtApePa": {
+                required: true
+            },
+            "ctl00$ContentPlaceHolder1$txtApeMa": {
+                required: true
+            },
+            "ctl00$ContentPlaceHolder1$txtFechaNac": {
+                required: true,
+                date: true
+            },
+            "ctl00$ContentPlaceHolder1$txtTelefono": {
+                required: true,
+                number: true
+            },
+            "ctl00$ContentPlaceHolder1$txtDireccion": {
+                required: true,
+            },
+            "ctl00$ContentPlaceHolder1$txtEmail": {
+                required: true,
+                email: true
             }
         }
     });
